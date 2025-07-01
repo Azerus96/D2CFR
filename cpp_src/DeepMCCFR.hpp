bf5c02a09e06c12704899fa47746267cec27bf39
Azerus96 <gencinski1996@gmail.com>
2025-07-01 05:02:04 +0300

    Update DeepMCCFR.hpp

--- cpp_src/DeepMCCFR.hpp
+++ cpp_src/DeepMCCFR.hpp
@@ -2,7 +2,7 @@
 #include "game_state.hpp"
 #include "hand_evaluator.hpp"
 #include "InferenceQueue.hpp"
-#include "SampleQueue.hpp" // <-- ДОБАВЛЕНО
+#include "SampleQueue.hpp"
 #include <vector>
 #include <map>
 #include <memory>
@@ -18,7 +18,7 @@ struct LocalSample {
 
 class DeepMCCFR {
 public:
-    // --- ИЗМЕНЕНИЕ: Принимаем указатель на новую очередь ---
+    // Конструктор принимает указатель на новую очередь для сэмплов
     DeepMCCFR(size_t action_limit, SampleQueue* sample_queue, InferenceQueue* inference_queue);
     ~DeepMCCFR(); 
     
@@ -28,15 +28,13 @@ private:
     void flush_local_buffer(); 
 
     HandEvaluator evaluator_;
-    // --- УДАЛЕНО: Прямой доступ к ReplayBuffer больше не нужен ---
-    // SharedReplayBuffer* replay_buffer_; 
-    SampleQueue* sample_queue_; // <-- ДОБАВЛЕНО
+    SampleQueue* sample_queue_;
     InferenceQueue* inference_queue_;
     size_t action_limit_;
     std::mt19937 rng_;
 
     std::vector<LocalSample> local_buffer_;
-    static constexpr size_t LOCAL_BUFFER_CAPACITY = 256; // Можно увеличить
+    static constexpr size_t LOCAL_BUFFER_CAPACITY = 256;
 
     std::map<int, float> traverse(GameState& state, int traversing_player);
     std::vector<float> featurize(const GameState& state, int player_view);
