f231fbd71270d888a148a555fb42ecb37fe4119f
Azerus96 <gencinski1996@gmail.com>
2025-07-02 06:25:07 +0300

    Update SampleQueue.hpp

--- cpp_src/SampleQueue.hpp
+++ cpp_src/SampleQueue.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include <vector>
-#include <thread> // Для std::this_thread::sleep_for
-#include <chrono> // Для std::chrono::microseconds
+#include <thread>
+#include <chrono>
+#include <atomic> // <-- ДОБАВЛЕНО
+
 #include "constants.hpp"
 #include "concurrentqueue.h"
 
@@ -16,25 +18,35 @@ struct SampleBatch {
 // Обертка над lock-free очередью с блокирующим pop
 class SampleQueue {
 public:
-    SampleQueue() {}
+    // ИЗМЕНЕНИЕ: Инициализируем флаг остановки
+    SampleQueue() : stop_flag_(false) {}
 
     void push(SampleBatch&& batch) {
         queue_.enqueue(std::move(batch));
     }
 
-    // Блокирующий pop
+    // ИЗМЕНЕНИЕ: Блокирующий pop с возможностью прерывания
     bool pop(SampleBatch& batch) {
         // try_dequeue неблокирующий, поэтому мы должны сами организовать ожидание
         while (!queue_.try_dequeue(batch)) {
+            // Если получили сигнал на остановку и очередь все еще пуста, выходим.
+            if (stop_flag_.load(std::memory_order_relaxed)) {
+                return false;
+            }
             // Если очередь пуста, немного поспим, чтобы не грузить CPU
-            // Это не идеально, но гораздо проще, чем condition_variable с этой библиотекой
             std::this_thread::sleep_for(std::chrono::microseconds(100));
         }
         return true;
     }
 
+    // ИЗМЕНЕНИЕ: Метод для установки флага остановки
+    void stop() {
+        stop_flag_.store(true, std::memory_order_relaxed);
+    }
+
 private:
     moodycamel::ConcurrentQueue<SampleBatch> queue_;
+    std::atomic<bool> stop_flag_; // <-- ДОБАВЛЕНО
 };
 
 }
